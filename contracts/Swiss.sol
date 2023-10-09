b5ed097ef2b37be59e71e0842c7802b222cb0bd3
KingzRex <toochukwukingz6@gmail.com>
2023-10-09 18:20:30 +0100

    first commit

---
+++ contracts/Swiss.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+contract SwissContract {
+    // Private state variable
+    string private data = "default"; // You can change this to a number if you prefer
+
+    // Function to set the state variable
+    function setData(string memory _data) public {
+        require(
+            keccak256(abi.encodePacked(_data)) !=
+                keccak256(abi.encodePacked(data)),
+            "New value must be different from the default value"
+        );
+        data = _data;
+    }
+
+    // Public function to return the current value of the state variable
+    function getData() public view returns (string memory) {
+        return data;
+    }
+}
